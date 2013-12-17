c25f56ae20c010a12a5c30fa11e418f144c61fab
Michael <Michael@5a6f51b7-912c-df4e-a7a2-906ca73860be>
2013-12-17 12:24:59 +0000

    Battle on MarsTest

---
+++ art/Packs/props/arena/ArenaSect2a.cs
@@ -0,0 +1,10 @@
+
+singleton TSShapeConstructor(ArenaSect2aDae)
+{
+   baseShape = "./ArenaSect2a.dae";
+   upAxis = "X_AXIS";
+   unit = "10";
+   singleDetailSize = "0";
+   adjustCenter = "1";
+   loadLights = "0";
+};
--- art/Worlds/MarsTest/config.cs
+++ art/Worlds/MarsTest/config.cs
@@ -2,6 +2,7 @@
 $AlterVerse::AvSet = "Base";
 $AlterVerse::ArtPacks = "trees/default" TAB
    "skies/default" TAB
+   "props/arena" TAB
    "AI/Gargoyle" TAB
    "AI/Tolgart" TAB
    "AI/Boglin" TAB
