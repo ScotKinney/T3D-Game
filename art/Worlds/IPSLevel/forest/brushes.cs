83c48c0f3da87abd078d7eb00dd8ea629a5dd0f1
Michael <Michael@5a6f51b7-912c-df4e-a7a2-906ca73860be>
2014-01-02 21:04:36 +0000

    Adding an empty forest to IPSLevel

---
+++ art/Worlds/IPSLevel/forest/brushes.cs
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------------------
+// Torque
+// Copyright GarageGames, LLC 2011
+//-----------------------------------------------------------------------------
+
+// This is the default save location for any ForestBrush(s) created in 
+// the Forest Editor.
+// This script is executed from ForestEditorPlugin::onWorldEditorStartup().
+
