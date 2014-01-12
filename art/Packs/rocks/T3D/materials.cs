8541467fe07db9950f469baf8a35259fb31a3caf
Michael <Michael@5a6f51b7-912c-df4e-a7a2-906ca73860be>
2014-01-12 14:27:19 +0000

    Putting needed materials back in

--- art/Packs/rocks/T3D/materials.cs
+++ art/Packs/rocks/T3D/materials.cs
@@ -1,4 +1,20 @@
 
+singleton Material(Rock2_mat)
+{
+	mapTo = "Rock2";
+
+	diffuseMap[0] = "Rock2.jpg";
+
+	diffuseColor[0] = "1 1 1 1";
+	specular[0] = "0.9 0.9 0.9 1";
+	specularPower[0] = 10;
+
+	doubleSided = false;
+	translucent = false;
+	translucentBlendOp = "None";
+   materialTag0 = "Rocks";
+};
+
 singleton Material(MossyRock02_mat)
 {
 	mapTo = "MossyRock02";
