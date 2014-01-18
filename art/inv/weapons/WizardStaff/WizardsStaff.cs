0dae34c5c8b1b93f15d9b423dca335791a9767e9
Scot <Scot@5a6f51b7-912c-df4e-a7a2-906ca73860be>
2014-01-18 18:45:44 +0000

    corrected damageStart and End nodes

--- art/inv/weapons/WizardStaff/WizardsStaff.cs
+++ art/inv/weapons/WizardStaff/WizardsStaff.cs
@@ -8,4 +8,5 @@ function WizardsStaffDts::onLoad(%this)
 {
    %this.addNode("Col-1", "", "0 0 0 0 0 1 0", "0");
    %this.addCollisionDetail("-1", "26-DOP", "WizardAStaff", "4", "30", "30", "32", "30", "30", "30");
+
 }
